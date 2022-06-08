1441c9c0053cae7f2f1cb456683688b01f293afe
Camilost1404 <camitorres1404@gmail.com>
2022-06-07 22:11:17 -0500

    Cambios finales

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,9 +2,13 @@ import React, { Component } from 'react'
 import Square from './Square'
 
 export class Board extends Component {
-    renderSquare(i) {
+    renderSquare(i, id) {
         return (
-            <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)}></Square>
+            <Square
+                id={'square' + id} value={this.props.squares[i]}
+                onClick={() => this.props.onClick(i)}
+                name={'button' + id}
+            />
         )
     }
 
@@ -12,19 +16,19 @@ export class Board extends Component {
         return (
             <div>
                 <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
+                    {this.renderSquare(0, 1)}
+                    {this.renderSquare(1, 2)}
+                    {this.renderSquare(2, 3)}
                 </div>
                 <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
+                    {this.renderSquare(3, 4)}
+                    {this.renderSquare(4, 5)}
+                    {this.renderSquare(5, 6)}
                 </div>
                 <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
+                    {this.renderSquare(6, 7)}
+                    {this.renderSquare(7, 8)}
+                    {this.renderSquare(8, 9)}
                 </div>
             </div>
         );
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Square(props) {
     return (
-        <button className="square" onClick={props.onClick}>
+        <button id={props.id} className="square" onClick={props.onClick} arial-label={props.name}>
             {props.value}
         </button>
     )
